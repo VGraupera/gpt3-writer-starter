4f4db6427f588e3a2576e31ee3c35bf844b6665d
Vidal Graupera <vgraupera@gmail.com>
2022-11-30 15:13:52 -0800

    tutorial done

---
+++ pages/api/generate.js
@@ -0,0 +1,59 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Write me a list of characters for a kids story with the title below.
+
+Title:
+`
+
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+  console.log("OpenAI replied...", basePromptOutput.text);
+
+
+  // I build Prompt #2.
+  const secondPrompt = 
+  `
+  Take the detailed list of characters and title below and generate a kids story in the style of Disney.
+
+  Title: ${req.body.userInput}
+
+  List of characters: ${basePromptOutput.text}
+
+  Story:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.8,
+		// I also increase max_tokens.
+    max_tokens: 2000,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
